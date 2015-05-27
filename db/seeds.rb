62ee1bf4af1446f8bae0c7b8a242e5312b9e5314
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-27 15:17:35 -0600

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.create(title: "ANOTHER EVENT FOR BIGS AND LITTLES ALIKE!", description: "Cotton candy cupcake lemon drops. Cheesecake danish caramels tiramisu ice cream gingerbread macaroon. Candy bonbon candy jelly-o. Halvah cotton candy bonbon muffin tart tootsie roll chocolate cake pudding jujubes.",
+            date: "1/2/2015", start_time: "01:00:00", end_time: "01:00:00", address: "4849 W. 30th Ave.", city: "Denver", state: "CO",
+            zipcode: 80546, image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStWP21np5FY0Al1AnqfDNO3P6A6eQKDqlepdS0X5QhxqUMs-l_")
+
+Event.create(title: "What!? ANOTHER EVENT FOR BIGS AND LITTLES!", description: "Cotton candy cupcake lemon drops. Cheesecake danish caramels tiramisu ice cream gingerbread macaroon. Candy bonbon candy jelly-o. Halvah cotton candy bonbon muffin tart tootsie roll chocolate cake pudding jujubes.",
+            date: "1/2/2015", start_time: "02:00:00", end_time: "01:00:00", address: "4849 W. 30th Ave.", city: "Denver", state: "CO",
+            zipcode: 90234, image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStWP21np5FY0Al1AnqfDNO3P6A6eQKDqlepdS0X5QhxqUMs-l_")
+
+Event.create(title: "ANOTHER EVENT FOR BIGS AND LITTLES ALIKE!", description: "Cotton candy cupcake lemon drops. Cheesecake danish caramels tiramisu ice cream gingerbread macaroon. Candy bonbon candy jelly-o. Halvah cotton candy bonbon muffin tart tootsie roll chocolate cake pudding jujubes.",
+            date: "1/2/2015", start_time: "01:00:00", end_time: "01:00:00", address: "4849 W. 30th Ave.", city: "Denver", state: "CO",
+            zipcode: 80212, image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStWP21np5FY0Al1AnqfDNO3P6A6eQKDqlepdS0X5QhxqUMs-l_")
+
+Event.create(title: "SOOOOOO MANY EVENTS!", description: "Cotton candy cupcake lemon drops. Cheesecake danish caramels tiramisu ice cream gingerbread macaroon. Candy bonbon candy jelly-o. Halvah cotton candy bonbon muffin tart tootsie roll chocolate cake pudding jujubes.",
+            date: "1/2/2015", start_time: "01:00:00", end_time: "01:00:00", address: "4849 W. 30th Ave.", city: "Denver", state: "CO",
+            zipcode: 56778, image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStWP21np5FY0Al1AnqfDNO3P6A6eQKDqlepdS0X5QhxqUMs-l_")
