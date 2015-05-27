0c2623d53942615af807b11dd3d24b7df5cda764
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-27 08:36:33 -0600

    add image_url to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525214916) do
+ActiveRecord::Schema.define(version: 20150527140256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150525214916) do
     t.string  "city"
     t.string  "state"
     t.integer "zipcode"
+    t.string  "image_url"
   end
 
   create_table "users", force: :cascade do |t|
