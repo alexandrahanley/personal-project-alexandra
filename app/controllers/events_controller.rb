6bd58d1df2c25250cba1c204bbc17026659699d4
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-27 15:16:55 -0600

    add all events controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,42 @@
 class EventsController < ApplicationController
 
-  def new
-    @event = Event.new
-  end
+  def show
+     @event = Event.find(params[:id])
+   end
 
+   def new
+     @event = Event.new
+   end
+
+   def edit
+     @event = Event.find(params[:id])
+   end
+
+   def create
+     @event = Event.new(event_params)
+
+     if @event.save
+       redirect_to @event
+     else
+       render 'new'
+     end
+   end
+
+   def update
+     @event = Event.find(params[:id])
+
+     if @event.update(event_params)
+       redirect_to @event
+     else
+       render 'edit'
+     end
+   end
+
+   def destroy
+     @event = Event.find(params[:id])
+     @event.destroy
+     redirect_to events_path
+   end
 
   private
 
