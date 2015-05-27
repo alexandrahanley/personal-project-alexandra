bc971673d0d4b936bb3c499302a5dedb893e1e2d
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-27 08:56:48 -0600

    add new action to event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,15 @@
 class EventsController < ApplicationController
 
+  def new
+    @event = Event.new
+  end
+
+
   private
 
   def event_params
     params.require(:event).permit(:title, :description, :date,
     :start_time, :end_time, :address, :city, :state, :zipcode)
   end
-  
+
 end
