f26ca54fbbfb36cceb04b6863e9b53c11ce8ea9a
Alexandra Hanley <alexandrahanley@gmail.com>
2015-05-28 23:32:28 -0600

    add user auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
    resources :events
 
+   get '/sign-up' => 'users#new', as: :signup
+   post '/sign-up' => 'users#create'
+   get 'sign-in' => 'sessions#new', as: :signin
+   post '/sign-in' => 'sessions#create'
+   get '/sign-out' => 'sessions#destroy', as: :signout
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,9 @@ Event.create(title: "ANOTHER EVENT FOR BIGS AND LITTLES ALIKE!", description: "C
 Event.create(title: "SOOOOOO MANY EVENTS!", description: "Cotton candy cupcake lemon drops. Cheesecake danish caramels tiramisu ice cream gingerbread macaroon. Candy bonbon candy jelly-o. Halvah cotton candy bonbon muffin tart tootsie roll chocolate cake pudding jujubes.",
             date: "1/2/2015", start_time: "01:00:00", end_time: "01:00:00", address: "4849 W. 30th Ave.", city: "Denver", state: "CO",
             zipcode: 56778, image_url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStWP21np5FY0Al1AnqfDNO3P6A6eQKDqlepdS0X5QhxqUMs-l_")
+
+User.create(first_name: "Alexandra", last_name: "Hanley", email: "alexandra@biglittlecolorado.org",
+            password: "password", admin: true)
+
+User.create(first_name: "Rachel", last_name: "Bell", email: "rachel@biglittlecolorado.org",
+            password: "password", admin: false)
