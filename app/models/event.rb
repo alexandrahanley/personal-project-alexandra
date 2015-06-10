1670e648b1bde70abc0b1f4532d64f29030d2693
Alexandra Hanley <alexandrahanley@gmail.com>
2015-06-09 19:46:09 -0600

    add external link to index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
-  validates :title, :description, :date, :start_time, :end_time, :end_time, :address, :city, :state, 
-  :zipcode, :latitude, :longitude, :avatar, presence: true
+  validates :title, :description, :date, :start_time, :end_time, :end_time, :address, :city, :state,
+  :zipcode, :avatar, presence: true
 
 
   def full_address
